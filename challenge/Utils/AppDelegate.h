30fc2aa7c18e3da107ffd2fd9c96d361958e47ce
ihsan husnul <ihsan.husnul@gmail.com>
2017-10-21 03:51:41 +0700

    init project template

---
+++ challenge/Utils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  challenge
+//
+//  Created by ihsan husnul on 10/21/17.
+//  Copyright © 2017 IHsan HUsnul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
